b6af380c023ab0bbc99f9380d6746252bed91099
Luan <luan.nascimento.dev@gmail.com>
2024-05-23 13:08:06 -0300

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://luan4560.github.io/luan45060.github.io/",
   plugins: [react()],
 });
